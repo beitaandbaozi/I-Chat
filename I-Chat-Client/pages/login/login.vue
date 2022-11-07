123b7b71fa89b4fb61445456a3b607d5d4087474
beitaandbaozi <2280496040@qq.com>
2022-11-07 16:57:11 +0800

    feat(client): 客户端配置(尚未完善)

---
+++ I-Chat-Client/pages/login/login.vue
@@ -0,0 +1,17 @@
+<template>
+	<view class="login">
+		<!-- 登录表单 -->
+		<!-- 注册表单 -->
+	</view>
+</template>
+
+<script setup lang="ts">
+	
+</script>
+
+<style lang="scss">
+.login {
+	height:100vh;
+	background: url('../../static/img/login-bg.jpg') no-repeat center;
+}
+</style>

3c269960296fdc203cab9660927fbbb2a99534bd
beitaandbaozi <2280496040@qq.com>
2022-11-10 11:31:32 +0800

    style(client): 登录页面背景图片各设备兼容

--- I-Chat-Client/pages/login/login.vue
+++ I-Chat-Client/pages/login/login.vue
@@ -12,7 +12,7 @@
 	import LoginByAccount from './components/login-by-account.vue'
 	import LoginByRegist from './components/login-by-regist.vue'
 	const currentComponet = ref < boolean > (true)
-	const handleChck = (value:boolean) => {
+	const handleChck = (value: boolean) => {
 		currentComponet.value = value
 	}
 </script>
@@ -23,6 +23,17 @@
 		flex-direction: column;
 		align-items: center;
 		height: 100vh;
-		background: url('../../static/img/login-bg.jpg') no-repeat center;
+		/* 加载背景图 */
+		background-image: url('../../static/img/login-bg.jpg');
+		/* 背景图垂直、水平均居中 */
+		background-position: center center;
+		/* 背景图不平铺 */
+		background-repeat: no-repeat;
+		/* 当内容高度大于图片高度时，背景图像的位置相对于viewport固定 */
+		background-attachment: fixed;
+		/* 让背景图基于容器大小伸缩 */
+		background-size: cover;
+		/* 设置背景颜色，背景图加载过程中会显示背景色 */
+		background-color: #464646;
 	}
 </style>

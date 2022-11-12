822c8c6ee012d2443d2e6385373f763f3838bdd4
beitaandbaozi <2280496040@qq.com>
2022-11-12 16:53:00 +0800

    feat(client): 消息、通讯录、发现、我的页面创建和tabBar注册

---
+++ I-Chat-Client/pages/chat-session/chat-session.vue
@@ -0,0 +1,22 @@
+<template>
+	<view class="chat-session">
+
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ I-Chat-Client/pages/community/community.vue
@@ -0,0 +1,22 @@
+<template>
+	<view class="community">
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>

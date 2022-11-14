6c31421b58ccc767313a637d8448924dc13e04c4
beitaandbaozi <2280496040@qq.com>
2022-11-14 23:04:26 +0800

    feat(serve): 账号注册，发送验证码业务

--- I-Chat-Serve/config.js
+++ I-Chat-Serve/config.js
@@ -29,4 +29,14 @@ module.exports = {
       };
     },
   },
+  // 邮箱发送模块配置
+  emailConfig: {
+    host: "smtp.qq.com", //邮箱服务器  这里我用的QQ邮箱
+    port: 465, //邮箱使用端口
+    secure: true, //是否使用默认的465端口
+    auth: {
+      user: "2280496040@qq.com", // 发送方邮箱地址
+      pass: "qypanpotyxdzdjac", // smtp 验证码
+    },
+  },
 };
--- I-Chat-Serve/routes/users.js
+++ I-Chat-Serve/routes/users.js
@@ -2,7 +2,12 @@ var express = require("express");
 const app = require("../app");
 var router = express.Router();
 const db = require("../db/index");
-const { info, msg } = require("../config");
+const { info, msg, emailConfig } = require("../config");
+// 邮箱发送模块
+const nodemailer = require("nodemailer");
+
+// 存储发送时的校验码
+const sessionEmail = [];
 
 // 根据邮件获取用户信息
 const getUserInfoByEmail = (email) => {
@@ -32,35 +37,83 @@ const newCreateToken = () => {
   return guid;
 };
 
+// 随机生成校验码
+const getCode = (num) => {
+  const all = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let result = "";
+  for (let i = 0; i < num; i++) {
+    let index = Math.floor(Math.random() * 62);
+    result += all.charAt(index);
+  }
+  return result;
+};
+
 // 登录
 router.post("/login", async (req, res) => {
   try {
     let { userName, password } = req.body;
     let queryResult = {
       myInfo: null,
-      token: ""
-    }
+      token: "",
+    };
     let result = await getUserInfoByEmail(userName);
     if (result.state) {
       if (result.data.length > 0) {
-        queryResult.myInfo = result.data[0]
+        queryResult.myInfo = result.data[0];
         if (queryResult.myInfo.Password == password) {
           queryResult.token = newCreateToken();
-          res.send(msg.success(queryResult, '成功'))
+          res.send(msg.success(queryResult, "成功"));
         } else {
-          res.send(msg.error('密码错误'));
+          res.send(msg.error("密码错误"));
           return;
         }
       } else {
-        res.send(msg.error('该用户不存在'));
+        res.send(msg.error("该用户不存在"));
         return;
       }
     } else {
-      res.send(msg.error('查询数据失败'));
+      res.send(msg.error("查询数据失败"));
       return;
     }
   } catch (err) {
     res.send(msg.error(err.message));
+  }
+});
+
+// 发送邮箱校验码
+router.post("/sendVerificationCode", async (req, res) => {
+  try {
+    let { email, timestamp } = req.body;
+    // 校验邮箱是否在本系统被注册过
+    let result = await getUserInfoByEmail(email);
+    if (result.state) {
+      if (result.data.length > 0) {
+        res.send(msg.error("该邮箱已经在系统注册过了"));
+        return;
+      } else {
+        // 创建发送邮件的请求对象
+        const transport = nodemailer.createTransport(emailConfig);
+        const verificationCode = getCode(6);
+        // 邮件信息
+        const mailObj = {
+          //发送方邮箱
+          from: `来自<${emailConfig.auth.user}>`,
+          to: email,
+          subject: "【贝塔-基于uniapp的聊天应用】账号注册",
+          // html格式
+          html: `您正在注册I-Chat账号，本次请求的邮件验证码是：<b>${verificationCode}</b>（如非本人操作，请忽略该信息）`,
+        };
+        // 发送邮件
+        transport.sendMail(mailObj);
+        // 发送这个时刻的校验码
+        sessionEmail[timestamp] = verificationCode;
+        res.send(msg.success(null, "发送验证码成功"));
+      }
+    } else {
+      res.send(msg.error("获取数据失败"));
+    }
+  } catch (error) {
+    res.send(msg.error(error.message));
   }
 });
 

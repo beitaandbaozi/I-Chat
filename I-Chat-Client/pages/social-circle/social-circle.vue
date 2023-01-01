e2c88a075fa6465696121fdfd3fa0eb1bb445967
beitaandbaozi <2280496040@qq.com>
2023-01-02 02:56:09 +0800

    style(client): 朋友圈页面，内容大致框架以及功能动画展示

--- I-Chat-Client/pages/social-circle/social-circle.vue
+++ I-Chat-Client/pages/social-circle/social-circle.vue
@@ -35,7 +35,40 @@
 				</view>
 			</view>
 			<view class="content">
+				<view class="social-content">
+					<view class="left">
+						<!-- 头像 -->
 
+					</view>
+					<view class="right">
+						<!-- 昵称 -->
+						<text class="nick-name">昵称</text>
+						<!-- 内容 -->
+						<!-- 文本内容 -->
+						<!-- 图片内容 -->
+						<view class="text-content">
+						</view>
+						<view class="image-content">
+						</view>
+						<!-- 点赞和评论功能 -->
+						<view class="options-flex">
+							<!-- 区域 -->
+							<template v-if="optionsFlag">
+								<view class="options-content">
+
+								</view>
+							</template>
+							<!-- 按钮 -->
+							<view class="options-btn" @click="handleOptions">
+								<svg class="icon img-options" aria-hidden="true">
+									<use xlink:href="#icon-danse_gongnengjihe-copy"></use>
+								</svg>
+							</view>
+						</view>
+
+						<!-- 点赞区和评论 -->
+					</view>
+				</view>
 			</view>
 		</scroll-view>
 	</view>
@@ -62,6 +95,12 @@
 	}
 	// 个人信息
 	const userInfo = computed(() => store.state.sender)
+
+	// 点赞和评论出现
+	const optionsFlag = ref < boolean > (false)
+	const handleOptions = () => {
+		optionsFlag.value = !optionsFlag.value
+	}
 </script>
 
 <style lang="scss">
@@ -149,8 +188,112 @@
 
 			.content {
 				width: 100%;
-				height: 2099rpx;
-				background: beige;
+				height: auto;
+
+				// width: 100%;
+				// height: 2099rpx;
+				// background: beige;
+				.social-content {
+					margin: 60rpx 40rpx 40rpx 40rpx;
+					// height:200rpx;
+
+					display: flex;
+
+					.left {
+						width: 60rpx;
+						height: 60rpx;
+						background-color: beige;
+					}
+
+					.right {
+						margin-left: 20rpx;
+
+						width: 100%;
+						border-bottom: 1px solid #F3F3F3;
+
+						display: flex;
+						flex-direction: column;
+						justify-content: center;
+
+
+						// 昵称
+						.nick-name {
+							color: rgb(87, 107, 149);
+							font-size: 15px;
+						}
+
+						// 文本内容
+						.text-content {
+							margin-top: 20rpx;
+
+							width: 100%;
+							height: 50rpx;
+							background-color: lightblue;
+						}
+
+						// 图片内容
+						.image-content {
+							margin-top: 10rpx;
+							width: 100%;
+							height: 100rpx;
+							background-color: lightcoral;
+							// flex 3张换一次
+						}
+
+						// 点赞和评论功能
+						.options-flex {
+							margin: 15rpx 0 20rpx 0;
+
+							height: 60rpx;
+
+							display: flex;
+							justify-content: flex-end;
+							align-items: center;
+
+							// 区域
+							.options-content {
+								margin-right: 20rpx;
+
+								width: 250rpx;
+								height: 60rpx;
+								background-color: rgb(76, 76, 76);
+								border-radius: 10rpx;
+								
+								animation: wakeup .2s ease;
+							}
+
+							@keyframes wakeup {
+								0% {
+									width: 50rpx;
+								}
+								100% {
+									width: 250rpx;
+								}
+							}
+
+							// 按钮
+							.options-btn {
+
+								width: 60rpx;
+								height: 40rpx;
+
+								background: rgb(247, 247, 247);
+
+								display: flex;
+								justify-content: center;
+								align-items: center;
+
+								.img-options {
+									width: 60rpx;
+									height: 45rpx;
+								}
+							}
+
+						}
+
+
+					}
+				}
 			}
 		}
 	}

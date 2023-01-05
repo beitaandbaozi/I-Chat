8934cdb3f9e58abe2bbc9459f8cbe11d5586cd5e
beitaandbaozi <2280496040@qq.com>
2023-01-05 23:20:05 +0800

    feat(client): 获取朋友圈数据接口

--- I-Chat-Serve/routes/community.js
+++ I-Chat-Serve/routes/community.js
@@ -1,7 +1,134 @@
 var express = require("express");
-const app = require("../app");
+const { msg } = require("../config");
 var router = express.Router();
 const db = require("../db/index");
 
+//!!! 从数据库获取朋友圈内容
+const getCommunityList = ({ pageIndex, pageSize }) => {
+  return new Promise((resolve, reject) => {
+    try {
+      db.query(
+        `select * from community order by Id desc limit ${
+          (pageIndex - 1) * pageSize
+        },${pageSize}`,
+        (error, res) => {
+          if (err) {
+            reject(info.error("获取朋友圈内容数据库查询失败", error));
+          } else {
+            resolve(info.success(res, "从数据库获取朋友圈内容成功！"));
+          }
+        }
+      );
+    } catch (error) {
+      reject(info.error("获取朋友圈内容数据库异常", error.message));
+    }
+  });
+};
+
+//!!! 从数据库获取朋友圈喜爱记录
+const getCommunityLikeRecord = ({ userId, communityId }) => {
+  return new Promise((resolve, reject) => {
+    try {
+      db.query(
+        `select * from community_like_record where UserId = ${userId} and CommunityId = ${communityId}`,
+        (error, res) => {
+          if (error) {
+            reject(info.error("获取朋友圈喜爱记录数据库查询失败", error));
+          } else {
+            resolve(info.success(res, "从数据库获取朋友圈喜爱记录成功！"));
+          }
+        }
+      );
+    } catch (error) {
+      reject(info.error("获取朋友圈喜爱记录异常", error.message));
+    }
+  });
+};
+
+//!!! 从数据库获取具体的朋友圈喜爱数量
+const getCommunityLikeCount = ({ communityId }) => {
+  return new Promise((resolve, reject) => {
+    try {
+      db.query(
+        `select * from community_like_record where CommunityId = ${communityId}`,
+        (error, res) => {
+          if (error) {
+            reject(info.error("获取具体的朋友圈喜爱数量数据库查询失败", error));
+          } else {
+            resolve(
+              info.success(res, "从数据库获取具体的朋友圈喜爱数量成功！")
+            );
+          }
+        }
+      );
+    } catch (error) {
+      reject(info.error("从数据库获取具体的朋友圈喜爱数量异常", error));
+    }
+  });
+};
+
+//!!! 从数据库获取朋友圈评论
+const getCommunityCommentList = ({ communityId }) => {
+  return new Promise((resolve, reject) => {
+    try {
+      db.query(
+        `select * from community_comment where CommunityId = ${communityId}`,
+        (error, res) => {
+          if (error) {
+            res.send(info.error("从数据库获取朋友圈评论数据库查询失败", error));
+          } else {
+            res.send(
+              info.success(res, "从数据库获取朋友圈评论数据库查询成功！")
+            );
+          }
+        }
+      );
+    } catch (error) {
+      reject(info.error("从数据库获取朋友圈评论数据库异常"));
+    }
+  });
+};
+
+// 获取朋友圈内容
+router.post("/getCommunityList", async (req, res) => {
+  try {
+    let model = req.body;
+    if (!(model.pageIndex > 0 && model.pageSize > 0)) {
+      res.send(msg.error("pageIndex和pageSize都必须大于0"));
+      return;
+    }
+    // *** 获取朋友圈内容
+    const communityList = await getCommunityList(model);
+    if (communityList.state) {
+      let queryData = communityList.data;
+      for (let i = 0; i < queryData.length; i++) {
+        queryData[i].ImgList = JSON.parse(queryData[i].ImgList);
+        // *** 获取朋友圈喜爱记录
+        let communityLikedRecord = await getCommunityLikeRecord({
+          userId: model.userId,
+          communityId: queryData[i].Id,
+        });
+        if (communityLikedRecord.state) {
+          queryData[i].IsLike =
+            communityLikedRecord.data?.length > 0 ? true : false;
+        }
+        // *** 获取朋友圈喜爱数量
+        let communityLikeCount = await getCommunityLikeCount({
+          communityId: queryData[i].Id,
+        });
+        queryData[i].LikeNum = communityLikeCount.data.length;
+        let communityCommentList = await getCommunityCommentList({
+          communityId: queryData[i].Id,
+        });
+        queryData[i].CommentList = communityCommentList.data;
+      }
+      res.send(msg.success(queryData, "获取朋友圈成功"));
+    } else {
+      res.send(msg.error("获取朋友圈内容失败"));
+    }
+  } catch (error) {
+    res.send(msg.error("获取朋友圈数据异常", error.message));
+  }
+});
 
 module.exports = router;
